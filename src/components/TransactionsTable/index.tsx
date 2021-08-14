dc24e5b8adfb61cd7204a0db03542f3d68022590
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-08-14 18:42:45 -0300

    Applying the react context API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from "react";
-
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import { Container } from "../TransactionsTable/styles";
 
-interface Transaction {
-  id: number;
-  title: string;
-  type: string;
-  category: string;
-  amount: number;
-  createdAt: string;
-}
-
 export function TransactionsTable() {
-  const [ transactions, setTransactions ] = useState<Transaction[]>([])
-
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions))
-  }, [])
+  const { transactions } = useTransactions()
 
   return (
     <Container>
